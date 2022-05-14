985060599aee5f7088746d9d199f910625427e40
Dan <danielhibbard1234@gmail.com>
2022-05-14 17:03:32 +0100

    First commit

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+  echo "<h1> First PHP site</h1>";
+  echo "<hr>";
+  echo "<p>Welcome to my site</p>";
+  ?>
+
+  </body>
+</html>

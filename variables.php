d557bc47cacc8c8ca9650cfe94166b55c1f6ca5d
Dan <danielhibbard1234@gmail.com>
2022-05-15 12:15:04 +0100

    Variables and user input practice

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+     $hello = "string";
+     echo ("This variable is $hello");
+     echo ("\n");
+     echo strlen($hello);
+
+
+     ?>
+
+
+
+  </body>
+</html>
